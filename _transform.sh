91b4930b3231148d735dd610a970ae02fd6d9555
ogl4jo3 <ogl4jo3@gmail.com>
2019-08-13 00:06:29 +0800

    Init.

---
+++ _transform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python ./src/arbitrary_image_stylization_with_weights.py \
+    --checkpoint=./checkpoints/model.ckpt \
+   	--output_dir=./images/output_dir \
+  	--style_images_paths=./images/style_images/*.jpg \
+  	--content_images_paths=./images/content_images/*.jpg \
+  	--image_size=256 \
+  	--content_square_crop=False \
+  	--style_image_size=256 \
+  	--style_square_crop=False \
+  	--logtostderr
